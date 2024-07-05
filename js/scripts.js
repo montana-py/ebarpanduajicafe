a8b3898baa98a35e74aa7256a079827d2dff1a26
Ebar Panduaji <ebarpanduaji1605@gmail.com>
2024-07-05 11:35:23 +0700

    menambahkan back to top

--- js/scripts.js
+++ js/scripts.js
@@ -53,9 +53,29 @@ window.addEventListener('DOMContentLoaded', event => {
 
 });
 
-// When the user clicks on the button, scroll to the top of the document
+var mybutton = document.getElementById("back-to-top");
 
-mybutton.addEventListener("click", function () {
-    document.body.scrollTop = 0; 
-    document.documentElement.scrollTop = 0; 
-});
+ // When the user scrolls down 20px from the top of the document, show the button
+
+    window.onscroll = function () {
+    scrollFunction();
+};
+
+    function scrollFunction() {
+        if (
+            document.body.scrollTop > 20 ||
+            document.documentElement.scrollTop > 20
+            ) {
+                mybutton.style.display = "block";
+                } else {
+                    mybutton.style.display = "none";
+
+                }
+    }
+
+ // When the user clicks on the button, scroll to the top of the document
+
+    mybutton.addEventListener("click", function () {
+        document.body.scrollTop = 0; 
+        document.documentElement.scrollTop = 0; 
+        });
